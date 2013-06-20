47e19fc9f13d6c38b4383c998111035956c0da05
Jared Hanson <jaredhanson@gmail.com>
2013-06-20 15:59:27 -0700

    Implement run and launch.

--- application.js
+++ application.js
@@ -4,8 +4,26 @@ function(Emitter, clazz) {
   
   function Application() {
     Application.super_.call(this);
+    this.rootController = undefined;
   }
   clazz.inherits(Application, Emitter);
+  
+  Application.prototype.run = function {
+    this.willLaunch();
+    this.launch();
+    this.didLaunch();
+  };
+  
+  Application.prototype.launch = function() {
+    if (!this.rootController) throw new Error('No root controller initialized by application.');
+    
+    this.rootController.willAddEl();
+    this.rootController.el.prependTo(document.body);
+    this.rootController.didAddEl();
+  };
+  
+  Application.prototype.willLaunch = function() {};
+  Application.prototype.didLaunch = function() {};
   
   return Application;
 });
